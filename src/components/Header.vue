990a0444674fbe7661c0e2d02ad9bac47e0f1b6d
D. Shreyder <schreyders@gmail.com>
2022-02-12 18:21:24 +0100

    #1: project created +Header component

---
+++ src/components/Header.vue
@@ -0,0 +1,16 @@
+<template lang="html">
+  <div class="header-text">
+      <p>Dmitriy Shreyder</p>
+      <p style="font-size: 18px;">IT Projekt Manager</p>
+  </div>
+  <img alt="Dmitriy Shreyder Foto 2019" class="introduction__photo" src="../../img/CV_Photo_2018_10.jpg" />
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
+
+<style lang="css" scoped>
+</style>
